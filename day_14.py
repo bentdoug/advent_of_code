e46720bae49afaeab61d9cea5cff11aab7585c94
Benjamin Douglas <74840985+bentdoug@users.noreply.github.com>
2023-12-14 18:40:24 -0500

    Update day_14.py - No more threading... close but not quite yet
    Found pattern, not sure how to use it

--- day_14.py
+++ day_14.py
@@ -1,56 +1,77 @@
-import time
-import concurrent.futures
-
-def rotate_matrix( m ):
+def rotate_matrix_counterclockwise( m ):
     """https://stackoverflow.com/questions/53250821/in-python-how-do-i-rotate-a-matrix-90-degrees-counterclockwise"""
     return [[m[j][i] for j in range(len(m))] for i in range(len(m[0])-1,-1,-1)]
 
-def shift_and_count(line):
-    total = 0
-    limit = 0
-    for idx in range(len(line)):
-        if line[idx] == "#":
-            limit = idx + 1
-        elif line[idx] == "O":
-            line[idx] = "."
-            line[limit] = "O"
-            total += (len(line)) - limit
-            #print((len(line)) - limit)
-            limit += 1
-    return total, line
+def rotate_matrix_clockwise( m ):
+    return [list(reversed([m[j][i] for j in range(len(m))])) for i in range(len(m[0]))]
 
-def roll_rocks(matrix):
+def calc_north_weight( m ):
     total = 0
-    with concurrent.futures.ThreadPoolExecutor(max_workers=len(matrix)) as executor:
-        future_to_line = {executor.submit(shift_and_count, matrix[idx]):idx for idx in range(len(matrix))}
-        #print(future_to_line)
-        for future in concurrent.futures.as_completed(future_to_line):
-            idx = future_to_line[future]
-            try:
-                data = future.result() # Should be line_total, newly arranged line
-                #print(data)
-                total += data[0]
-                matrix[idx] = data[1]
-            except Exception as exc:
-                print('Line %r generated an exception: %s' % (idx, exc))
-            else:
-                pass
-    return matrix, total
+    m = rotate_matrix_counterclockwise(m)
+    for row in m:
+        for idx in range(len(row)):
+            if row[idx] == "O":
+                total += len(row) - idx
+
+    return total
+
+def tilt( m ):
+    m = rotate_matrix_counterclockwise(m)
+    total = 0
+
+    for line in m:
+        #print(line)
+        limit = 0
+        for idx in range(len(line)):
+            if line[idx] == "#":
+                limit = idx + 1
+            elif line[idx] == "O":
+                line[idx] = "."
+                line[limit] = "O"
+                total += (len(line)) - limit
+                #print((len(line)) - limit)
+                limit += 1
+    return total, rotate_matrix_clockwise(m)
+
 
 input_file = open("Advent_of_Code\day_14_input.small", "r")
 lines = input_file.readlines()
+# get rid of \n chars
+for line in range(len(lines)):
+    lines[line] = lines[line][:-1]
 
-matrix = lines
-times = {}
-start = time.time()
-for _ in range(1):
+lines_orig = lines
+
+results = []
+matches = []
+ctr = 0
+while len(matches) < 1:
+    ctr += 1
+    #if ctr %10 == 0:
+        #print(ctr)
+    if lines in results:
+        matches.append([results.index(lines), ctr])
+    results.append(lines)
+
+    # Succesful Cycle
+    for _ in range(4):
+        total, lines = tilt(lines)
+        lines = rotate_matrix_clockwise(lines)
+
+spacing = matches[0][1]-matches[0][0]+2
+print(spacing, 1000000000%spacing)
+num_loops = (1000000000%spacing) + matches[0][0]
+
+lines = lines_orig
+
+print("Num loops : ", num_loops, "Match: ", matches[0])
+for _ in range(num_loops):
+    # Succesful Cycle
     for x in range(4):
-        matrix = rotate_matrix(matrix)
-        matrix, total = roll_rocks(matrix)
-for i in matrix:
-    print(i)
-end = time.time()
-times=(end-start)
-print(times)
+        total, lines = tilt(lines)
+        lines = rotate_matrix_clockwise(lines)
+        print(total)
+    total = calc_north_weight(lines)
+    print("FOUR SET: {}".format(total), "LOOP NUM: {}".format(_))
 
 print(total)
